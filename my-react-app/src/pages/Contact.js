6cda945be3cc88440cd5c2831243974483013558
Noé Libon <n.libon@students.ephec.be>
2022-02-19 09:57:16 +0100

    css alignment

--- my-react-app/src/pages/Contact.js
+++ my-react-app/src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 class Contact extends React.Component {
 
@@ -9,11 +10,37 @@ class Contact extends React.Component {
     };
 
     render() {
+        const STYLES = {
+            layout: {
+                padding: "10px"
+            },
+            contentTables: {
+                textAlign: "center",
+                marginLeft: "auto",
+                marginRight: "auto",
+                borderWidth: "1px",
+                borderStyle: "solid",
+                borderColor: "black"
+            }
+        }
         return (
-            <div>
-                <p>Contact page</p>
+            <div style={STYLES.contentTables}>
+                <div id="contactTitle">
+                    <p>Liste de mes contacts</p>
+                </div>
+                <div id="contactList">
+                    <table style={STYLES.contentTables}>
+                        <tr>
+                            <td>My picture</td>
+                            <td>Noé</td>
+                        </tr>
+                    </table>
+                </div>
+                <br/>
+                <div id="contactAddition">
+                    <Button variant="dark" as="input" type="submit" value="Ajouter un contact" />
+                </div>
             </div>
-
         );
     };
 }
