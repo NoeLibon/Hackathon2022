3db9c211772640817af3f8720e88436fac45d1c0
Noé Libon <n.libon@students.ephec.be>
2022-02-19 03:21:17 +0100

    css beginning

--- my-react-app/src/pages/Call.js
+++ my-react-app/src/pages/Call.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 class Call extends React.Component {
 
@@ -9,9 +10,48 @@ class Call extends React.Component {
     };
 
     render() {
+        const STYLES = {
+            contentDiv: {
+                backgroundColor: "grey",
+            },
+            contentMargin: {
+                marginLeft: "10px",
+                width: "100%",
+            },
+            contentAlign: {
+                textAlign: "center",
+            },
+            contentColumn: {
+                columns: 2,
+            },
+            contentRow: {
+                rows: 2,
+            },
+        };
         return (
             <div>
                 <p>Call page</p>
+                <div style={STYLES.contentAlign}>
+                    <div id="firstName" style={STYLES.contentDiv}>
+                        Casse-Couilles
+                    </div>
+                    <br/>
+                    <div id="interaction">
+                        <div id="columns" style={STYLES.contentColumn}>
+                            <section id="otherCamera" style={STYLES.contentDiv}>
+                                coucou
+                            </section>
+                            <section id="rightSide" style={STYLES.contentRow}>
+                                <section id="myCamera" style={STYLES.contentDiv}>
+                                    coucou
+                                </section>
+                                <section id="chat" style={STYLES.contentDiv}>
+                                    bonjour
+                                </section>
+                            </section>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         );
