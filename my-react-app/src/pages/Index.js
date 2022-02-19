a00c43adabd2fd2733443291ec7550b3553205dd
severinRobert <sevrobe21@gmail.com>
2022-02-19 02:07:40 +0100

    react route working

---
+++ my-react-app/src/pages/Index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Index extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Index page</p>
+      </div>
+
+    );
+  };
+}
+
+export default Index;
